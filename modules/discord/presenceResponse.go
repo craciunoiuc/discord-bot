6acfda6d3c99b3839bc50a0b0967b8807c3a7d99
Denis Ehorovici <dehorovici@gmail.com>
2022-08-02 22:19:15 +0300

    Adjust PresenceResponse to cache previous status of a user

--- modules/discord/presenceResponse.go
+++ modules/discord/presenceResponse.go
@@ -1,18 +1,34 @@
 package discord
 
 import (
+	"fmt"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/craciunoiuc/discord-bot/spec"
 )
+
+var prevStatusMap map[string]discordgo.Status
+
+func init() {
+	prevStatusMap = make(map[string]discordgo.Status)
+}
 
 func PresenceResponse(s *discordgo.Session, m *discordgo.PresenceUpdate) {
 	if m.User.ID == s.State.User.ID || !userIsCringeMaster(m.User.ID) {
 		return
 	}
 
+	prevStatus, found := prevStatusMap[m.User.ID]
+
 	if m.Status == discordgo.StatusOnline {
-		s.ChannelMessageSend(spec.Cfg.DiscordCfg.GuildMainChannelId, "Păzea")
-	} else if m.Status == discordgo.StatusOffline {
+		if found && prevStatus == discordgo.StatusOnline {
+			s.ChannelMessageSend(spec.Cfg.DiscordCfg.GuildMainChannelId, fmt.Sprintf("<@%s> lasă telefonul!", m.User.ID))
+		} else if !found || prevStatus == discordgo.StatusOffline || prevStatus == discordgo.StatusInvisible {
+			s.ChannelMessageSend(spec.Cfg.DiscordCfg.GuildMainChannelId, "Păzea")
+		}
+	} else if (!found || prevStatus == discordgo.StatusOnline) && m.Status == discordgo.StatusOffline {
 		s.ChannelMessageSend(spec.Cfg.DiscordCfg.GuildMainChannelId, "Doamne-ajută")
 	}
+
+	prevStatusMap[m.User.ID] = m.Status
 }
