747e447a1b528d9c14ca3b9084dbe4fb28bc6719
Denis Ehorovici <dehorovici@gmail.com>
2023-02-19 21:07:46 +0200

    Generalize "loliteas" detection

--- modules/discord/funnyUtils.go
+++ modules/discord/funnyUtils.go
@@ -13,9 +13,12 @@ import (
 
 func handleFunnyMessages(s *discordgo.Session, m *discordgo.MessageCreate) {
 	normalizationTransform := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
+
 	normalizedMessage, _, _ := transform.String(normalizationTransform, m.Content)
-	normalizedMessageWithoutHyphens := strings.ReplaceAll(normalizedMessage, "-", "")
-	if messageIsLoliteas(normalizedMessageWithoutHyphens) {
+	lowerCaseNormalizedMessage := strings.ToLower(normalizedMessage)
+	lowerCaseNormalizedMessageWithoutHyphens := strings.ReplaceAll(lowerCaseNormalizedMessage, "-", "")
+
+	if messageIsLoliteas(lowerCaseNormalizedMessageWithoutHyphens) {
 		gifUrl := "https://tenor.com/view/frog-frog-dance-rana-ficcata-rana-pazza-gif-23866513"
 
 		message, error := s.ChannelMessageSend(m.ChannelID, gifUrl)
@@ -26,5 +29,5 @@ func handleFunnyMessages(s *discordgo.Session, m *discordgo.MessageCreate) {
 }
 
 func messageIsLoliteas(message string) bool {
-	return strings.Contains(message, "loliteas") || strings.Contains(message, "teas") && strings.Contains(message, "loli")
+	return strings.Contains(message, "loli") || strings.Contains(message, "lolesc")
 }
